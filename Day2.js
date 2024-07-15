e7843109a381b00ce8a7ec8a2d979f5e50e8f598
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-15 22:51:22 +0530

    Day2

--- Day2.js
+++ Day2.js
@@ -1,25 +1,79 @@
-// Task 1
-// let a = 10;
-// let b = 20;
-// console.log(a+b);
+/*
+Task 1
+let a = 10;
+let b = 20;
+console.log(a+b);
 
-// Task 2
-// let a = 20;
-// let b = 10;
-// console.log(a-b);
+Task 2
+let a = 20;
+let b = 10;
+console.log(a-b);
 
-// Task 3
-// let a = 10;
-// let b = 20;
-// console.log(a*b);
+Task 3
+let a = 10;
+let b = 20;
+console.log(a*b);
 
-// Task 4
-// let a = 10;
-// let b = 20;
-// console.log(a/b);
+Task 4
+let a = 10;
+let b = 20;
+console.log(a/b);
 
-// Task 5
-// let a = 20;
-// let b = 10;
-// console.log(a%b);
+Task 5
+let a = 20;
+let b = 10;
+console.log(a%b);
 
+Task 6
+let a = 10;
+a += 20;
+console.log(a);
+
+Task 7
+let a = 10;
+a -= 5;
+console.log(a);
+
+Task 8
+let a = 10;
+let b = 20;
+console.log(a>b);
+
+let a = 10;
+let b = 20;
+console.log(a<b);
+
+Task 9
+let a = 10;
+let b = 20;
+console.log(a>=10 && b>=20);
+
+Task 10
+let a = 10;
+let b = "10";
+console.log(a==b);
+console.log(a===b);
+
+Task 11
+let a = 8;
+let b = 19;
+console.log(a>=10 || b>=20);
+
+Task 12
+let a = true;
+console.log(!a);
+
+Task 13
+let a = 10;
+let b = 20;
+console.log(a>=10 && b>=20);
+
+Task 14
+let a = 5;
+if(a > 0){
+    console.log("Positive");
+}
+else{
+    console.log("Negative");
+}
+*/
