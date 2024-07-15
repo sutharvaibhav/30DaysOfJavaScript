538c7dae94aca860a4c5be4b4a3055277f44f4a0
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-15 21:15:02 +0530

    day2

---
+++ Day2.js
@@ -0,0 +1,25 @@
+// Task 1
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+
+// Task 2
+// let a = 20;
+// let b = 10;
+// console.log(a-b);
+
+// Task 3
+// let a = 10;
+// let b = 20;
+// console.log(a*b);
+
+// Task 4
+// let a = 10;
+// let b = 20;
+// console.log(a/b);
+
+// Task 5
+// let a = 20;
+// let b = 10;
+// console.log(a%b);
+
