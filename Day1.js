35b13322c3a11a762592704520750a3f8ad9855d
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-15 23:37:14 +0530

    Day1

---
+++ Day1.js
@@ -0,0 +1,41 @@
+/*
+Task 1
+var n = 1000;
+console.log(n);
+
+Task 2
+let num = "Vaibhav";
+console.log(num);
+
+Task 3
+const bool = true;
+console.log(bool);
+
+Task 4
+let num = 123;
+console.log(typeof(num));
+let str = "Hello";
+console.log(typeof(str));
+let bool = true;
+console.log(typeof(bool));
+let car = {
+  Brand: "Hyundai",
+  Model: "EON",
+  Version: 2016,
+  Color: "Grey"
+};
+console.log(typeof(car));
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(typeof(arr));
+
+Task 5
+let num = 123;
+console.log(num);
+let num = 456;
+console.log(num);
+
+Task 6
+const val = 123;
+const val = 321;
+Uncaught SyntaxError: Identifier 'val' has already been declared
+*/
