a29d0108ca8f41aae457021d0e30c5c43693762c
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-15 23:35:05 +0530

    Day3

--- Day3.js
+++ Day3.js
@@ -1,1 +1,101 @@
-jshfisdihdfhdsjpf
+/*
+Task 1
+let a = 5;
+if(a>0){
+    console.log("Positive");
+}
+else if(a<0){
+    console.log("Negative");
+}
+else{
+    console.log("Zero");
+}
+
+Task 2
+let age = 17;
+if(age >= 18){
+    console.log("You can vote!");
+}
+else{
+    console.log("You cannot vote!");
+}
+
+Task 3
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is greatest");
+}
+else if(num2 > num1 && num2 > num3){
+    console.log("num2 is greatest");
+}
+else{
+    console.log("num3 is greatest");
+}
+
+Task 4
+let day = 2;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Sunday");
+}
+
+Task 5
+let grade = "a";
+
+switch (grade) {
+  case "a":
+    console.log("90%");
+    break;
+  case "b":
+    console.log("80%");
+    break;
+  case "c":
+    console.log("70%");
+    break;
+  case "d":
+    console.log("60%");
+    break;
+  default:
+    console.log("Fail");
+}
+
+Task 6
+let num = 5;
+if( num % 2 == 0){
+    console.log("number is even");
+}
+else{
+    console.log("number is odd");
+}
+
+Task 7
+let year = 1999;
+if ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
+    console.log("Leap Year");   
+}
+else{
+    console.log("Not a leap year!")
+}
+*/
