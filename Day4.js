84883c74498fc4515132094980f0a2a28fe696d3
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-16 21:55:49 +0530

    Day4

---
+++ Day4.js
@@ -0,0 +1,50 @@
+/*
+Task 1
+for (let num = 1; num <= 10; num++) {
+  console.log(num);
+}
+
+Task 2
+for (let a = 1; a <= 10; a++) {
+  console.log(5 * a);
+}
+
+Task 3
+let b = 1;
+while(b<=5){
+    console.log(b);
+    b++;
+}
+
+Task 4
+let c = 10;
+while (c > 0) {
+  console.log(c);
+  c--;
+}
+
+Task 5
+let d = 1;
+do {
+  console.log(d++);
+} while (d <= 5);
+
+Task 6
+let n = num = 10;
+let fact = 1;
+do{
+    fact = fact * n;
+    --n;
+    if(n==0){
+        console.log(num,' factorial is ', fact);
+    }
+}while(n>=1);
+
+Task 7
+for(let r = 1 ; r<=5; r++){
+    for(let c = 1; c<=r ;c++){
+        console.log("*");
+    }
+    console.log("\n");
+}
+*/
