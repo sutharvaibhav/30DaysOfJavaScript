f849398edaf73a48960f25ae1988910e8bfa7918
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-19 00:00:37 +0530

    Day6

---
+++ Day6.js
@@ -0,0 +1,78 @@
+// Task 1
+// let arr = [1,2,3,4,5];
+// for(let r = 0; r<arr.length; r++) console.log(arr[r]);
+
+// Task 2
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   if (i == 0 || i == arr.length - 1) console.log(arr[i]);
+// }
+
+// Task 3
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+// arr.push(6);
+// console.log(arr);
+
+// Task 4
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+// arr.push(6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// Task 5
+// let arr = [1,2,3,4,5];
+// arr.shift();
+// console.log(arr);
+
+// Task 6
+// let arr = [1,2,3,4,5];
+// arr.shift();
+// console.log(arr);
+// arr.unshift(1);
+// console.log(arr);
+
+// Task 7
+// let arr = [4,9,16,25,36];
+// let newArr = arr.map(Math.sqrt)
+// console.log(newArr);
+
+// Task 8
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age%2==0;
+// }
+// console.log(result);
+
+// Task 9
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age%2==0;
+// }
+// console.log(result);
+
+// Task 10
+// let arr = [1,2,3,4,5];
+// for(let r = 0; r<arr.length; r++) console.log(arr[r]);
+
+// Task 11
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+// numbers.forEach((a) => {
+//     sum += a; 
+// });
+// console.log(sum);
+
+// Task 12
+// let arr = [[1,2],[3,4],[5,6]];
+// console.log(arr);
+
+// Task 13
+// let arr = [[1,2],[3,4],[5,6]];
+// console.log(arr[2][1]);
