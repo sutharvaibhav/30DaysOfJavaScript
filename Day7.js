1e48e256bf8c0b31373414640d095bd9b30c9ded
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-20 00:00:31 +0530

    Day7

---
+++ Day7.js
@@ -0,0 +1,104 @@
+// Task 1
+// let book = {
+//     title: "Rudest book ever",
+//     page: 250,
+//     price: 150
+// }
+// console.log(book);
+
+// Task 2
+// let book = {
+//     title: "Rudest book ever",
+//     page: 250,
+//     price: 150
+// }
+// console.log(book.title);
+// console.log(book.page);
+// console.log(book.price);
+
+// Task 3
+// let book = {
+//     title: "Rudest book ever",
+//     page: 250,
+//     price: 150,
+//     learn: func = () => {
+//         return book.title+" 's cost is "+ book.price;
+//     }
+// }
+// console.log(book.learn());
+
+// Task 4
+// let book = {
+//     title: "Rudest book ever",
+//     page: 250,
+//     price: 150,
+//     learn: func = () => {
+//         return book.title+" 's cost is "+ book.price;
+//     },
+//     update: upd = (a) => {
+//         return book.price = a;
+//     }
+// }
+// book.update(500)
+// console.log(book.price);
+
+// Task 5
+// let book = {
+//     obj1: book1 = {
+//         name: "Book1",
+//         price: 150
+//     },
+//     obj2: book2 = {
+//         name: "Book2",
+//         price: 250
+//     },
+//     obj3: book3 = {
+//         name: "Book3",
+//         price: 350
+//     },
+// }
+// console.log(book.obj1);
+
+// Task 6
+// let book = {
+//     obj1: book1 = {
+//         name: "Book1",
+//         price: 150
+//     },
+//     obj2: book2 = {
+//         name: "Book2",
+//         price: 250
+//     },
+//     obj3: book3 = {
+//         name: "Book3",
+//         price: 350
+//     },
+// }
+// console.log(book.obj1.name);
+
+// Task 7
+// let myBook = {
+//     title: "Book1",
+//     price: 99,
+// }
+// myBook.bookInfo = function(){
+//     return this.title;
+// }
+// myBook.bookInfo();
+
+// Task 8
+// let myBook = {
+//     title: "Book1",
+//     price: 99,
+// }
+// for(let key in myBook){
+//     console.log(key + " = " + myBook[key]);
+// }
+
+// Task 9
+// let myBook = {
+//   title: "Book1",
+//   price: 99,
+// };
+// Object.keys(myBook);
+// Object.values(myBook);
