da9251d29a503d8316ae00f36523c4aefe30964c
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-20 13:57:40 +0530

    Day8

---
+++ Day8.js
@@ -0,0 +1,54 @@
+// Task 1
+// let name = `Vaibhav`;
+// let age = 23;
+// console.log(`Hello! My name is ${name}. I'm ${age} years old.`);
+
+// Task 2
+// let name = `Vaibhav`;
+// let age = 23;
+// console.log(`Hello! My name is ${name}. \n I'm ${age} years old.`);
+
+// Task 3
+// const arr = [1,2,3,4,5];
+// const [firstElement, secondElement] = arr;
+// console.log(firstElement);
+// console.log(secondElement);
+
+// Task 4
+// let person = {
+//     name: "Vaibhav",
+//     age: 23,
+// };
+// let { name: personName, age: personAge } = person;
+
+// Task 5
+// let arr = [1,2,3,4,5];
+// let newArr = [...arr,6,7,8,9];
+// console.log(newArr);
+
+// Task 6
+// let arr = [1,2,3,4];
+// let newArr = [...arr];
+// newArr.push(5);
+// console.log(newArr);
+
+// Task 7
+// let product = (a,b = 1) => {
+//     return a*b;
+// }
+// console.log(product(5));
+
+// Task 8
+// let name = "Vaibhav";
+// let age = 23;
+// let person = {name,age}
+// console.log(person);
+
+// Task 9
+// let LAST_NAME = "lastName";
+// let myObj = {
+//     firstName: "Vaibhav",
+//     [LAST_NAME]: "Suthar",
+// }
+// console.log(`Hello! My name is ${myObj.firstName} ${myObj.lastName}`);
+
