e933ecb9da5f784d68f05393bff64033f3ace115
Vaibhav <vaibhavsuthar33@gmail.com>
2024-07-21 23:37:08 +0530

    Day9

---
+++ Day9.js
@@ -0,0 +1,62 @@
+// Task 1
+
+document.getElementById("hero-section").style.display = "none";
+
+// Task 2
+
+document.getElementById("hero-section").style.backgroundColor = "red";
+
+// Task 3
+
+let div = document.createElement("div");
+div.id = "myDiv";
+div.textContent = "Hello this is my div";
+div.style.backgroundColor = "red";
+div.style.height = "100px";
+document.body.appendChild(div);
+
+// Task 4
+
+var ul = document.createElement("ul");
+for (let i = 1; i <= 3; i++) {
+  let li = document.createElement("li");
+  li.textContent = `Item ${i}`;
+  ul.appendChild(li);
+}
+document.body.appendChild(ul);
+
+// Task 5
+let hero = document.getElementById("hero-section");
+hero.remove();
+
+// Task 6
+let hero = document.getElementById("hero-section");
+var lastChild = document.getElementById("hero-section").lastChild;
+if (lastChild != null) {
+  hero.removeChild(lastChild);
+}
+
+// Task 7
+let indus = document.getElementById("indus");
+indus.textContent = "Companies";
+
+// Task 8
+let serve = document.getElementById("serve");
+serve.style = none;
+
+// Task 9
+let targetPara = document.getElementById("targetPara");
+let targetBtn = document.getElementById("targetBtn");
+targetBtn.addEventListener("click", function () {
+  targetPara.style.color = "red";
+});
+
+// Task 10
+let servBt = document.getElementById('servBt');
+let targetImg= document.getElementById("targetImg");
+servBt.addEventListener("mouseover", function(){
+  targetImg.style.border ="1px solid green";
+});
+
+
+
